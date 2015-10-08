5ef277f743b75499e21a584470746503fce4a247
claudine_sinclair <femtobyte@gmail.com>
2015-10-08 16:08:01 -0700

    Initial Commit

---
+++ tapper2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapper2
+//
+//  Created by C Sinclair on 10/8/15.
+//  Copyright © 2015 mingdyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
